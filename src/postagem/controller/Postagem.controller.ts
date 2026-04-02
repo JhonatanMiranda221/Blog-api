3b879d97661d2dc10de0fa7fa514790998722a06
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-04-02 10:36:37 -0300

    atualizacao controller e service

--- src/postagem/controller/Postagem.controller.ts
+++ src/postagem/controller/Postagem.controller.ts
@@ -1,13 +1,54 @@
-import {Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
-import {Postagem} from "../entities/postagem.entity";
-import {PostagemService} from "../services/postagem.service"
+import {
+    Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from '../services/postagem.service';
 
-@Controller ("/Postagens")
+//Porta de entrada,
+// Recebe a requisição HTTP, valida os dados de entrada,
+// e chama o Service. Não tem lógica de negócio aqui.
+@Controller('/Postagens')
 export class PostagemController {
-    constructor(private readonly postagemService: PostagemService) {}
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Postagem[]> {
-        return this.postagemService.findAll();
-    }
+  constructor(private readonly postagemService: PostagemService) {}
+
+@Get()
+@HttpCode(HttpStatus.OK)
+findAll(): Promise<Postagem[]> {
+    return this.postagemService.findAll();
+  }
+
+@Get('/:id')
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.postagemService.findById(id);
+  }
+@Get('/titulo/:titulo')
+@HttpCode(HttpStatus.OK)
+  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
+    return this.postagemService.findAllByTitulo(titulo);
+}
+
+@Post()
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number){
+    return
+  }
+@Put()
+@HttpCode(HttpStatus.OK)
+update (@Body() postagem: Postagem ): Promise<Postagem> {
+  return this.postagemService.update(postagem)
+}
 }

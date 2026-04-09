cd946ca207bf7d9aacf9c612c86b8dddaf5d7333
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-04-09 20:49:50 -0300

    Criação Autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import {Tema} from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module'; 
+import { AuthModule } from './auth/auth.module'
+import { UsuarioModule } from './usuario/usuario.module';
+import { Usuario } from './usuario/entities/usuario.entity';
 
 @Module({
   imports: [
@@ -16,11 +19,13 @@ import { TemaModule } from './tema/tema.module';
       username: 'root', // username of database
       password: 'root', // password of database
       database: 'db_blogpessoal', // name of database
-      entities: [Postagem, Tema], // entities of database
+      entities: [Postagem, Tema, Usuario], // entities of database
       synchronize: true, // synchronize database
     }),
     PostagemModule,
-    TemaModule
+    TemaModule,
+    AuthModule,
+    UsuarioModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/postagem/controller/Postagem.controller.ts
+++ src/postagem/controller/Postagem.controller.ts
@@ -1,44 +1,41 @@
-import {
-    Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  ParseIntPipe,
-  Post,
-  Put,
-} from '@nestjs/common';
-import { Postagem } from '../entities/postagem.entity';
-import { PostagemService } from '../services/postagem.service';
+import { Controller, Get, Post, Put, Delete, HttpCode, HttpStatus, Param, Body, HttpException, UseGuards, ParseIntPipe } from "@nestjs/common";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
+import { Postagem } from "../entities/postagem.entity";
+import { PostagemService } from "../services/postagem.service";
 
-//Porta de entrada,
-// Recebe a requisição HTTP, valida os dados de entrada,
-// e chama o Service. Não tem lógica de negócio aqui.
-@Controller('/postagens')
+@UseGuards(JwtAuthGuard)
+@Controller("/postagens")
 export class PostagemController {
-  constructor(private readonly postagemService: PostagemService) {}
+  constructor(private readonly postagemService: PostagemService) { }
 
-@Get()
-@HttpCode(HttpStatus.OK)
-findAll(): Promise<Postagem[]> {
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
   }
 
-@Get('/titulo/:titulo')
-@HttpCode(HttpStatus.OK)
-  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
-    return this.postagemService.findAllByTitulo(titulo);
-}
   @Get('/:id')
-  findById(@Param('id', ParseIntPipe) id: number) {
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
     return this.postagemService.findById(id);
   }
 
-@Post()
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findAllByTitulo(titulo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() postagem: Postagem): Promise<Postagem> {
     return this.postagemService.create(postagem);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.update(postagem);
   }
 
   @Delete('/:id')
@@ -46,9 +43,5 @@ findAll(): Promise<Postagem[]> {
   delete(@Param('id', ParseIntPipe) id: number){
     return this.postagemService.delete(id);
   }
-@Put()
-@HttpCode(HttpStatus.OK)
-update (@Body() postagem: Postagem ): Promise<Postagem> {
-  return this.postagemService.update(postagem)
-}
+
 }

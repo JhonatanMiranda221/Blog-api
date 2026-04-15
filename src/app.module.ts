759ffd6a2965ca446faff4c4521c5b11e8367af5
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-04-15 20:35:21 -0300

    Atualização do Deploy - Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import {Tema} from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module'; 
 import { AuthModule } from './auth/auth.module'
 import { UsuarioModule } from './usuario/usuario.module';
